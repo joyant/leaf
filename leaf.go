f5d899609c2429da350322811429b1a2330ff135
zhaijinghui <zhaijinghui@xiaoyezi.com>
2021-06-30 16:32:15 +0800

    modify log

--- leaf.go
+++ leaf.go
@@ -2,24 +2,21 @@ package leaf
 
 import (
 	"github.com/joyant/leaf/cluster"
-	"github.com/joyant/leaf/conf"
 	"github.com/joyant/leaf/console"
 	"github.com/joyant/leaf/log"
 	"github.com/joyant/leaf/module"
-	"os"
-	"os/signal"
 )
 
 func Run(mods ...module.Module) {
 	// logger
-	if conf.LogLevel != "" {
-		logger, err := log.New(conf.LogLevel, conf.LogPath, conf.LogFlag)
-		if err != nil {
-			panic(err)
-		}
-		log.Export(logger)
-		defer logger.Close()
-	}
+	//if conf.LogLevel != "" {
+	//	logger, err := log.New(conf.LogLevel, conf.LogPath, conf.LogFlag)
+	//	if err != nil {
+	//		panic(err)
+	//	}
+	//	log.Export(logger)
+	//	defer logger.Close()
+	//}
 
 	log.Release("Leaf %v starting up", version)
 
@@ -36,10 +33,16 @@ func Run(mods ...module.Module) {
 	console.Init()
 
 	// close
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt, os.Kill)
-	sig := <-c
-	log.Release("Leaf closing down (signal: %v)", sig)
+	//c := make(chan os.Signal, 1)
+	//signal.Notify(c, os.Interrupt, os.Kill)
+	//sig := <-c
+	//log.Release("Leaf closing down (signal: %v)", sig)
+	//console.Destroy()
+	//cluster.Destroy()
+	//module.Destroy()
+}
+
+func Stop()  {
 	console.Destroy()
 	cluster.Destroy()
 	module.Destroy()
--- log/log.go
+++ log/log.go
@@ -1,15 +1,5 @@
 package log
 
-import (
-	"errors"
-	"fmt"
-	"log"
-	"os"
-	"path"
-	"strings"
-	"time"
-)
-
 // levels
 const (
 	DebugLevel   = 0
@@ -30,87 +20,87 @@ type Logger interface {
 	DoPrintf(level int, printLevel string, format string, a ...interface{})
 }
 
-type Log struct {
-	level      int
-	baseLogger *log.Logger
-	baseFile   *os.File
-}
-
-func New(strLevel string, pathname string, flag int) (Logger, error) {
-	// level
-	var level int
-	switch strings.ToLower(strLevel) {
-	case "debug":
-		level = DebugLevel
-	case "release":
-		level = ReleaseLevel
-	case "error":
-		level = ErrorLevel
-	case "fatal":
-		level = FatalLevel
-	default:
-		return nil, errors.New("unknown level: " + strLevel)
-	}
-
-	// logger
-	var baseLogger *log.Logger
-	var baseFile *os.File
-	if pathname != "" {
-		now := time.Now()
-
-		filename := fmt.Sprintf("%d%02d%02d_%02d_%02d_%02d.log",
-			now.Year(),
-			now.Month(),
-			now.Day(),
-			now.Hour(),
-			now.Minute(),
-			now.Second())
-
-		file, err := os.Create(path.Join(pathname, filename))
-		if err != nil {
-			return nil, err
-		}
-
-		baseLogger = log.New(file, "", flag)
-		baseFile = file
-	} else {
-		baseLogger = log.New(os.Stdout, "", flag)
-	}
-
-	// new
-	logger := new(Log)
-	logger.level = level
-	logger.baseLogger = baseLogger
-	logger.baseFile = baseFile
-
-	return logger, nil
-}
-
-// It's dangerous to call the method on logging
-func (logger *Log) Close() {
-	if logger.baseFile != nil {
-		logger.baseFile.Close()
-	}
-
-	logger.baseLogger = nil
-	logger.baseFile = nil
-}
-
-func (logger *Log) DoPrintf(level int, printLevel string, format string, a ...interface{}) {
-	if level < logger.level {
-		return
-	}
-	if logger.baseLogger == nil {
-		panic("logger closed")
-	}
-
-	format = printLevel + format
-	logger.baseLogger.Output(3, fmt.Sprintf(format, a...))
-
-	if level == FatalLevel {
-		os.Exit(1)
-	}
-}
+//type Log struct {
+//	level      int
+//	baseLogger *log.Logger
+//	baseFile   *os.File
+//}
+//
+//func New(strLevel string, pathname string, flag int) (Logger, error) {
+//	// level
+//	var level int
+//	switch strings.ToLower(strLevel) {
+//	case "debug":
+//		level = DebugLevel
+//	case "release":
+//		level = ReleaseLevel
+//	case "error":
+//		level = ErrorLevel
+//	case "fatal":
+//		level = FatalLevel
+//	default:
+//		return nil, errors.New("unknown level: " + strLevel)
+//	}
+//
+//	// logger
+//	var baseLogger *log.Logger
+//	var baseFile *os.File
+//	if pathname != "" {
+//		now := time.Now()
+//
+//		filename := fmt.Sprintf("%d%02d%02d_%02d_%02d_%02d.log",
+//			now.Year(),
+//			now.Month(),
+//			now.Day(),
+//			now.Hour(),
+//			now.Minute(),
+//			now.Second())
+//
+//		file, err := os.Create(path.Join(pathname, filename))
+//		if err != nil {
+//			return nil, err
+//		}
+//
+//		baseLogger = log.New(file, "", flag)
+//		baseFile = file
+//	} else {
+//		baseLogger = log.New(os.Stdout, "", flag)
+//	}
+//
+//	// new
+//	logger := new(Log)
+//	logger.level = level
+//	logger.baseLogger = baseLogger
+//	logger.baseFile = baseFile
+//
+//	return logger, nil
+//}
+//
+//// It's dangerous to call the method on logging
+//func (logger *Log) Close() {
+//	if logger.baseFile != nil {
+//		logger.baseFile.Close()
+//	}
+//
+//	logger.baseLogger = nil
+//	logger.baseFile = nil
+//}
+//
+//func (logger *Log) DoPrintf(level int, printLevel string, format string, a ...interface{}) {
+//	if level < logger.level {
+//		return
+//	}
+//	if logger.baseLogger == nil {
+//		panic("logger closed")
+//	}
+//
+//	format = printLevel + format
+//	logger.baseLogger.Output(3, fmt.Sprintf(format, a...))
+//
+//	if level == FatalLevel {
+//		os.Exit(1)
+//	}
+//}
 
 //func (logger *Log) Debug(format string, a ...interface{}) {
 //	logger.DoPrintf(DebugLevel, PrintDebugLevel, format, a...)
@@ -128,7 +118,9 @@ func (logger *Log) DoPrintf(level int, printLevel string, format string, a ...in
 //	logger.DoPrintf(FatalLevel, PrintFatalLevel, format, a...)
 //}
 
-var gLogger, _ = New("debug", "", log.LstdFlags)
+//var gLogger, _ = New("debug", "", log.LstdFlags)
+
+var gLogger Logger
 
 // It's dangerous to call the method on logging
 func Export(logger Logger) {
